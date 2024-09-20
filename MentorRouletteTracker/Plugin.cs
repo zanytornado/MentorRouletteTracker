5c67257e8ce28c2bafefeb2b05e7bab2751a178c
zanytornado <82668072+zanytornado@users.noreply.github.com>
2024-09-20 13:55:52 -0500

    updated some basic plugin info

--- MentorRouletteTracker/Plugin.cs
+++ MentorRouletteTracker/Plugin.cs
@@ -14,7 +14,7 @@ public sealed class Plugin : IDalamudPlugin
     [PluginService] internal static ITextureProvider TextureProvider { get; private set; } = null!;
     [PluginService] internal static ICommandManager CommandManager { get; private set; } = null!;
 
-    private const string CommandName = "/pmycommand";
+    private const string CommandName = "/mrt";
 
     public Configuration Configuration { get; init; }
 
@@ -37,7 +37,7 @@ public sealed class Plugin : IDalamudPlugin
 
         CommandManager.AddHandler(CommandName, new CommandInfo(OnCommand)
         {
-            HelpMessage = "A useful message to display in /xlhelp"
+            HelpMessage = "Opens the main plugin UI"
         });
 
         PluginInterface.UiBuilder.Draw += DrawUI;
--- MentorRouletteTracker/Windows/MainWindow.cs
+++ MentorRouletteTracker/Windows/MainWindow.cs
@@ -14,10 +14,8 @@ public class MainWindow : Window, IDisposable
     private Plugin Plugin;
 
     // We give this window a hidden ID using ##
-    // So that the user will see "My Amazing Window" as window title,
-    // but for ImGui the ID is "My Amazing Window##With a hidden ID"
     public MainWindow(Plugin plugin, string goatImagePath)
-        : base("My Amazing Window##With a hidden ID", ImGuiWindowFlags.NoScrollbar | ImGuiWindowFlags.NoScrollWithMouse)
+        : base("Mentor Roulette Tracker##MentorRouletteTracker", ImGuiWindowFlags.NoScrollbar | ImGuiWindowFlags.NoScrollWithMouse)
     {
         SizeConstraints = new WindowSizeConstraints
         {
